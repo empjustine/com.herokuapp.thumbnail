a5eb3e5492a2fa68e170b844926786d556c0256f
emp.justine <emp.justine@gmail.com>
2013-10-24 14:30:41 -0200

    v1
    * now parsing UCS URLs;
    * now with default white background.

--- Gemfile
+++ Gemfile
@@ -2,4 +2,6 @@ source 'https://rubygems.org'
 
 gem 'sinatra'
 gem 'mini_magick'
+
 gem 'rest-client'
+gem 'addressable'
--- thumbnail.rb
+++ thumbnail.rb
@@ -1,6 +1,8 @@
 require 'sinatra'
 require 'mini_magick'
+
 require 'rest-client'
+require 'addressable/uri'
 
 
 before do
@@ -17,16 +19,22 @@ end
 
 get '/:height/:width/*/*' do
 
-  url = params[:splat].join '://'
+  raw_url = params[:splat].join '://'
+  url = Addressable::URI.parse(raw_url).normalize.to_str
 
   blob = RestClient.get url
   image = MiniMagick::Image.read blob
 
   halt 500 unless image.valid?
 
+  image.background '#ffffff'
+  image.flatten
   image.resize "#{params[:height]}x#{params[:width]}>"
   image.format 'jpeg'
   image.strip
+
+  image.fill 'none'
+  image.draw 'matte 0,0 floodfill'
 
   content_type :jpeg
 
